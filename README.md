befd239cb6a0a074b09fb33324ce3a12d78a12f4
i0Ek3 <kno30826@gmail.com>
2021-04-15 20:40:24 +0800

    Update hust

--- README.md
+++ README.md
@@ -6,13 +6,25 @@
 
 - `brew install rust` // for macOS
 - `rustc xxx.rs` // compile rust source
+    - or run command: `./hust xxx`
 - `cargo new xxx` // create a new project
 - `cargo build`
 - `cargo run`
 
 ## YSK
 
-coming soon...
+- all values have its own variable, we call it owner
+- slice in Rust same with Go
+- `struct Xxx()`
+- `#[derive(Debug)]` // debug libaray
+- `enum Xxx {}` with `match`
+- `enum Option<T> {}`
+- `if let value = var { // body }`
+- Crate/Package/Module
+    - `use` keyword
+    - `::`
+    - `mod module_name`
+
 
 ## Credit
 
--- example.rs
+++ example.rs
@@ -1,11 +1,44 @@
+// reference standard libaray
+use std::f64::consts::PI;
+
+// import module
+mod hust_mod;
+
+// enable debug mode
+#[derive(Debug)]
+
+// define struct
+struct Triangle {
+    a: u32,
+    b: u32,
+    c: u32,
+}
+
 fn h(hi: String) {
     println!("{}", hi);
 }
 
+// use keyword for module
+use crate::hust_mod::hust::_1::_1f;
+use crate::hust_mod::hust::_1::_1f as f1;
+
 fn main() {
     let hi = "Hi rust";
-    let i: i32 = 0;
-    for i in 0..7 {
+    let _i: i32 = 0;
+    for _i in 0..7 {
         h(hi.to_string());
     }
+
+    let str = String::from(hi);
+    let slice = &str[..2];
+    println!("{}", slice);
+
+    let t = Triangle{ a: 10, b: 20, c: 30 };
+    println!("t is {:?}", t);
+
+    //println!("{}", hust_mod::hust::_1::_1f())
+    println!("{}", _1f());
+    println!("{}", f1());
+
+    println!("{}", (PI * 4.0 / 3.0).cos());
 }
